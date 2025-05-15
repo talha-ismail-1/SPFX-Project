3bdcf04697811fa76790cc7090eddf35dfa43c47
Hammad Ayaz <hammad.ayaz@centric.ae>
2025-05-15 17:58:55 +0500

    comit

---
+++ src/webparts/orgChartPrerequisites/components/IOrgChartPrerequisitesProps.ts
@@ -0,0 +1,7 @@
+export interface IOrgChartPrerequisitesProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/orgChartPrerequisites/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IOrgChartPrerequisitesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'OrgChartPrerequisitesWebPartStrings' {
+  const strings: IOrgChartPrerequisitesWebPartStrings;
+  export = strings;
+}
